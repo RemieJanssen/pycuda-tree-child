324396e850f2c75d952dad1419c564539bfcbabf
Remie Janssen <remie.janssen@rivm.nl>
2025-09-17 17:27:53 +0200

    WIP

--- pycuda_tree_child.py
+++ pycuda_tree_child.py
@@ -1,24 +1,76 @@
 import pycuda.autoinit
 import pycuda.driver as drv
 import numpy
+import networkx as nx
+import phylox as px
+import datetime
 
 from pycuda.compiler import SourceModule
+
 mod = SourceModule("""
-__global__ void multiply_them(float *dest, float *a, float *b)
+__global__ void is_tree_child(int max_number_of_neighbors, int *nodes, bool result)
 {
-  const int i = threadIdx.x;
-  dest[i] = a[i] * b[i];
+    const int node_index = threadIdx.x;
+    bool node_is_tree_child = false;
+    bool has_out_neighbor = false;
+    for (int out_nb_index = (2 * node_index + 1) * max_number_of_neighbors; out_nb_index < (2 * node_index + 2) * max_number_of_neighbors); out_nb_index++){
+        if (out_nb_index == -1){
+            continue;
+        }
+        has_out_neighbor = true;
+        if (!is_retic(max_number_of_neighbors, nodes, out_nb_index){
+            node_is_tree_child = true;
+        }
+    }
+    if (node_is_tree_child or !has_out_neighbor){
+        result = true;
+    }
+}
+
+__device__ bool is_retic(int max_number_of_neighbors, int *nodes, int node_index)
+{
+    int number_of_in_neighbours = 0;
+    for (int in_nb_index = (2 * node_index) * max_number_of_neighbors; in_nb_index < (2 * node_index + 1) * max_number_of_neighbors); in_nb_index++){
+        if (in_nb_index != -1){
+            number_of_in_neighbours++;
+    return number_of_in_neighbours > 1;
 }
 """)
 
-multiply_them = mod.get_function("multiply_them")
 
-a = numpy.random.randn(400).astype(numpy.float32)
-b = numpy.random.randn(400).astype(numpy.float32)
+device_is_tree_child = mod.get_function("is_tree_child")
 
-dest = numpy.zeros_like(a)
-multiply_them(
-        drv.Out(dest), drv.In(a), drv.In(b),
-        block=(400,1,1), grid=(1,1))
+def network_to_adjacency_list(network, max_degree=2):
+    relabeled_network  = nx.convert_node_labels_to_integers(network)
+    node_list = numpy.full(len(relabeled_network.nodes)*2*max_degree, -1)
+    for node in relabeled_network.nodes():
+        in_neighbour_index = 2*node*max_degree
+        for i, neighbour in relabeled_network.predecessors(node):
+            node_list[in_neighbour_index+i] = neighbour
+        out_neighbour_index = (2*node+2)*max_degree
+        for i, neighbour in relabeled_network.successors(node):
+            node_list[out_neighbour_index+i] = neighbour
+    return node_list
 
-print(dest-a*b)
+
+def is_tree_child(network):
+    result = False
+    max_numer_of_neighbours = 2
+    nodes = network_to_adjacency_list(network, max_degree=max_numer_of_neighbours)
+    # a = numpy.random.randn(400).astype(numpy.int32)
+    # b = numpy.random.randn(400).astype(numpy.int32)
+
+    # dest = numpy.zeros_like([0])
+
+    device_is_tree_child(
+        drv.In(max_numer_of_neighbours), drv.In(nodes), drv.Out(result),
+        block=(len(network.nodes()),1,1), grid=(1,1))
+    return result
+
+
+if __name__ == "__main__":
+    network = px.generators.lgt.base.generate_network_lgt_conditional(100,10,0.1)
+    print(network)
+    print(datetime.datetime.now())
+    print(is_tree_child(network))
+    print(datetime.datetime.now())
